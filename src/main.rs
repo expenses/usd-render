e9c8dc9484c3915a1ea727e29195e84e692fcc63
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-01-24 01:09:48 +1300

    Basic movement

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,120 @@
+use bbl_usd::{tf, usd, vt};
+use dolly::prelude::*;
+use glfw::{fail_on_errors, Action, Context, Key};
 use glow::HasContext;
 
 fn main() {
-    let (gl, window, event_loop) = unsafe {
-        let event_loop = glutin::event_loop::EventLoop::new();
-        let window_builder = glutin::window::WindowBuilder::new()
-            .with_title("Hello triangle!")
-            .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
-        let window = glutin::ContextBuilder::new()
-            .with_vsync(true)
-            .build_windowed(window_builder, &event_loop)
-            .unwrap()
-            .make_current()
-            .unwrap();
-        let gl = glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
-        (gl, window, event_loop)
-    };
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
 
-    let mut imaging = std::ptr::null_mut();
+    let (mut window, events) = glfw
+        .create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
 
-    dbg!(unsafe { bbl_usd::ffi::usdImaging_GLEngine_new(&mut imaging) });
+    window.make_current();
+    window.set_key_polling(true);
 
-    let stage = bbl_usd::usd::Stage::open(std::env::args().nth(1).unwrap()).unwrap();
+    let gl =
+        unsafe { glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _) };
+
+    let engine = usd::GLEngine::new();
+
+    let stage = usd::Stage::open(std::env::args().nth(1).unwrap()).unwrap();
 
     let prim = stage.pseudo_root();
-    let camera_prim = stage.prim_at_path("/camera1").unwrap();
 
-    dbg!(imaging);
-
-    let mut camera = std::ptr::null_mut();
-    let mut gf_camera = std::ptr::null_mut();
-    let mut frustum = std::ptr::null_mut();
-    let mut proj = bbl_usd::ffi::gf_Matrix4d_t {
-        m: Default::default(),
-    };
-
-    let mut view = bbl_usd::ffi::gf_Matrix4d_t {
-        m: Default::default(),
-    };
+    let mut camera: CameraRig = CameraRig::builder()
+        .with(Position::new(glam::Vec3::splat(2.0)))
+        .with(YawPitch {
+            yaw_degrees: 45.0,
+            pitch_degrees: -45.0,
+        })
+        .with(Smooth::new_position_rotation(1.0, 0.1))
+        .build();
 
     unsafe {
-        bbl_usd::ffi::usdGeom_Camera_new(camera_prim.ptr(), &mut camera);
-        bbl_usd::ffi::usdGeom_Camera_GetCamera(
-            camera,
-            &bbl_usd::ffi::usd_TimeCode_t {
-                time: Default::default()
-            },
-            &mut gf_camera,
-        );
-        bbl_usd::ffi::gf_Camera_GetFrustum(gf_camera, &mut frustum);
-        bbl_usd::ffi::gf_Frustum_ComputeProjectionMatrix(frustum, &mut proj);
-        bbl_usd::ffi::gf_Frustum_ComputeViewMatrix(frustum, &mut view);
+        gl.clear_color(0.1, 0.2, 0.3, 1.0);
     }
 
-    dbg!(camera, gf_camera);
+    let params = usd::GLRenderParams::new();
 
-    unsafe {
-        use glutin::event::{Event, WindowEvent};
-        use glutin::event_loop::ControlFlow;
+    let mut size = window.get_size();
+    engine.set_render_viewport(glam::DVec4::new(0.0, 0.0, size.0 as _, size.1 as _));
 
-        gl.clear_color(0.1, 0.2, 0.3, 1.0);
+    let mut grab_toggled = false;
+    let mut prev_cursor_pos = glam::DVec2::from(window.get_cursor_pos());
 
-        event_loop.run(move |event, _, control_flow| {
-            *control_flow = ControlFlow::Wait;
+    while !window.should_close() {
+        glfw.poll_events();
+
+        for (_, event) in glfw::flush_messages(&events) {
             match event {
-                Event::LoopDestroyed => {
-                    return;
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
                 }
-                Event::MainEventsCleared => {
-                    window.window().request_redraw();
-                }
-                Event::RedrawRequested(_) => {
-                    gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
-
-                    bbl_usd::ffi::usdImaging_GLEngine_SetCameraState(imaging, &view, &proj);
-
-                    let viewport = glam::DVec4::new(
-                        0.0,
-                        0.0,
-                        window.window().inner_size().width as _,
-                        window.window().inner_size().height as _,
-                    );
-
-                    bbl_usd::ffi::usdImaging_GLEngine_SetRenderViewport(
-                        imaging,
-                        &viewport as *const glam::DVec4 as *const _,
-                    );
-
-                    bbl_usd::ffi::usdImaging_render(imaging, prim.ptr());
-
-                    dbg!(window.window().inner_size());
-                    window.swap_buffers().unwrap();
-                }
-                Event::WindowEvent { ref event, .. } => match event {
-                    WindowEvent::Resized(physical_size) => {
-                        window.resize(*physical_size);
+                glfw::WindowEvent::Key(Key::G, _, Action::Press, _) => {
+                    grab_toggled = !grab_toggled;
+                    if grab_toggled {
+                        window.set_cursor_mode(glfw::CursorMode::Disabled);
+                    } else {
+                        window.set_cursor_mode(glfw::CursorMode::Normal);
                     }
-                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                    _ => (),
-                },
-                _ => (),
+                }
+                _ => {}
             }
-        });
+        }
+
+        let cursor_pos = glam::DVec2::from(window.get_cursor_pos());
+        let delta = cursor_pos - prev_cursor_pos;
+        prev_cursor_pos = cursor_pos;
+
+        let current_size = window.get_size();
+        if current_size != size {
+            size = current_size;
+            engine.set_render_viewport(glam::DVec4::new(0.0, 0.0, size.0 as _, size.1 as _));
+        }
+
+        let mut movement = glam::IVec3::ZERO;
+
+        movement.x += (window.get_key(glfw::Key::D) != glfw::Action::Release) as i32;
+        movement.x -= (window.get_key(glfw::Key::A) != glfw::Action::Release) as i32;
+
+        movement.y += (window.get_key(glfw::Key::W) != glfw::Action::Release) as i32;
+        movement.y -= (window.get_key(glfw::Key::S) != glfw::Action::Release) as i32;
+
+        movement.z += (window.get_key(glfw::Key::Q) != glfw::Action::Release) as i32;
+        movement.z -= (window.get_key(glfw::Key::Z) != glfw::Action::Release) as i32;
+
+        let movement = movement.as_vec3() * 0.1;
+
+        let movement = movement.x * camera.final_transform.right()
+            + movement.y * camera.final_transform.forward()
+            + movement.z * glam::Vec3::Y;
+
+        camera.driver_mut::<Position>().translate(movement);
+
+        if grab_toggled {
+            let yaw_pitch = -delta * 0.25;
+            camera
+                .driver_mut::<YawPitch>()
+                .rotate_yaw_pitch(yaw_pitch.x as _, yaw_pitch.y as _);
+        }
+
+        let transform = camera.update(1.0 / 60.0);
+        let view = glam::DMat4::look_at_rh(
+            transform.position.as_dvec3(),
+            transform.position.as_dvec3() + transform.forward().as_dvec3(),
+            transform.up().as_dvec3(),
+        );
+        let proj = glam::DMat4::perspective_rh_gl(59.0_f64.to_radians(), 1.0, 0.01, 1000.0);
+
+        engine.set_camera_state(view, proj);
+
+        unsafe {
+            gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
+
+            engine.render(&prim, &params);
+        }
+
+        window.swap_buffers();
     }
 }
