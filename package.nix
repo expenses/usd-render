18fce0713bce38cf330b9c615680ffeb046c476a
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-11 13:53:30 +1300

    Clean up

--- package.nix
+++ package.nix
@@ -1,20 +1,14 @@
-{ lib, stdenv, craneLib, pkg-config, cmake, ninja, glib, gtk3, babble, gcc, clang, boost
-, tbb, iconv, darwin, openusd-minimal
-}:
+{ lib, stdenv, craneLib, pkg-config, cmake, ninja, gtk3, babble, gcc, clang
+, iconv, darwin, openusd-minimal }:
 craneLib.buildPackage {
   src = craneLib.cleanCargoSource (craneLib.path ./.);
   strictDeps = true;
 
-  LIBCLANG_PATH="${clang.cc.lib}/lib";
+  LIBCLANG_PATH = "${clang.cc.lib}/lib";
 
   nativeBuildInputs = [ pkg-config cmake ninja gcc clang ];
 
-  buildInputs = [
-    babble
-    boost
-    tbb
-    openusd-minimal
-  ] ++ lib.optionals stdenv.isDarwin
+  buildInputs = [ babble openusd-minimal ] ++ lib.optionals stdenv.isDarwin
     ([ iconv ] ++ (with darwin.apple_sdk.frameworks; [ Carbon Cocoa Kernel ]))
     ++ lib.optionals stdenv.isLinux ([ gtk3 ]);
 }
