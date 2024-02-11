1bddcbd312dc80041f98bb3084cc0e0fd75664ab
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-11 13:40:04 +1300

    Get working

--- package.nix
+++ package.nix
@@ -1,15 +1,19 @@
-{ lib, stdenv, craneLib, pkg-config, cmake, ninja, glib, gtk3, babble, gcc
-, python311Packages, tbb, iconv, darwin, openusd-minimal
-, useMinimalUsd ? true }:
+{ lib, stdenv, craneLib, pkg-config, cmake, ninja, glib, gtk3, babble, gcc, clang, boost
+, tbb, iconv, darwin, openusd-minimal
+}:
 craneLib.buildPackage {
   src = craneLib.cleanCargoSource (craneLib.path ./.);
   strictDeps = true;
 
-  nativeBuildInputs = [ pkg-config cmake ninja gcc ];
+  LIBCLANG_PATH="${clang.cc.lib}/lib";
+
+  nativeBuildInputs = [ pkg-config cmake ninja gcc clang ];
 
   buildInputs = [
     babble
-    (if useMinimalUsd then openusd-minimal else python311Packages.openusd)
+    boost
+    tbb
+    openusd-minimal
   ] ++ lib.optionals stdenv.isDarwin
     ([ iconv ] ++ (with darwin.apple_sdk.frameworks; [ Carbon Cocoa Kernel ]))
     ++ lib.optionals stdenv.isLinux ([ gtk3 ]);
